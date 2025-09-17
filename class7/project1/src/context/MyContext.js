baf0cf88aa9a77b3818b8e30c2dbb5ebb1872139
Vijayvishvakarma0000 <vijayvishvakarma0000@gmail.com>
2025-09-17 23:53:27 +0530

    Add files via upload
    it use  for context api

---
+++ class7/project1/src/context/MyContext.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function MyContext() {
+    const MyContext= createContext();
+  return (
+    <div>
+        MyContext
+
+
+    </div>
+  )
+}
+
+export default MyContext
