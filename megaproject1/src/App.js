4b6a0a96d405bfa2f334361317e84afd6748138e
Vijayvishvakarma0000 <vijayvishvakarma0000@gmail.com>
2025-09-24 21:35:33 +0530

    Add files via upload

---
+++ megaproject1/src/App.js
@@ -0,0 +1,45 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup"
+import About from "./pages/About.jsx"
+import Category from "./pages/Category.jsx"
+import LaptopDetail from "./pages/LaptopDetail.jsx"
+import NotFound from "./pages/NotFound"
+import Profile from "./pages/Profile.jsx"
+import ResetPassword from "./pages/ResetPassword.jsx"
+
+import SingleLaptopInfo from "./pages/SingleLaptopInfo.jsx";
+import { useState } from "react";
+
+
+function App() {
+  const [product, setProduct] = useState({});
+  function handleProductSingle(data) {
+    setProduct(data)
+  }
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/category" element={<Category />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/profile/reset-password" element={<ResetPassword />} />
+        <Route path="/laptop-detail/category/:categoryName/:id" element={<LaptopDetail handleProductSingle={handleProductSingle} />} />
+        <Route path="/laptop-detail/category/:categoryName/:id/product/:productName/:pid" element={<SingleLaptopInfo product={product} />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
